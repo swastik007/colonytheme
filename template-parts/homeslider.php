ac8a770a5395379f67c65dba1be97e65b0d02060
swastik <swastik@kodiary.com>
2025-05-10 20:29:53 +0545

    chagnes

--- template-parts/homeslider.php
+++ template-parts/homeslider.php
@@ -1,39 +1,36 @@
 <!-- Hero Section -->
-<div class="custom-slider owl-carousel owl-theme" id="home">
-  <?php
-  $slides = [
-    [
-      'title' => 'Scale Your Business with Us',
-      'desc' => 'We provide the tools, partners, and supply chain solutions to grow faster.',
-      'img' => 'img3.jpg',
-      'alt' => 'Business team working together'
-    ],
-    [
-      'title' => 'Trusted Partners & Suppliers',
-      'desc' => 'From sourcing to delivery, we connect you with the best in the industry.',
-      'img' => 'img1.jpg',
-      'alt' => 'Supplier logistics'
-    ],
-    [
-      'title' => 'Fast & Reliable Delivery',
-      'desc' => 'Optimize your operations with seamless supply chain support.',
-      'img' => 'img2.jpg',
-      'alt' => 'Logistics and shipping'
-    ]
-  ];
-
-  foreach ($slides as $slide): ?>
-    <div class="slide">
-      <div class="box d-flex flex-column flex-md-row align-items-center justify-content-between">
-        <div class="slide-text">
-          <h1><?= $slide['title'] ?></h1>
-          <p><?= $slide['desc'] ?></p>
-          <!-- <a href="#" class="btn btn-primary">Get Started</a> -->
-        </div>
-        <div class="slide-image">
-          <img src="<?= get_template_directory_uri(); ?>/assets/images/<?= $slide['img'] ?>" alt="<?= $slide['alt'] ?>" class="img-fluid">
-        </div>
+<div class="custom-slider" id="home">
+  <div class="slide">
+    <div class="box">
+      <div class="slide-text">
+        <h1>Scale Your Business with Us</h1>
+        <p>We provide the tools, partners, and supply chain solutions to grow faster.</p>
+      </div>
+      <div class="slide-image">
+        <img src="<?php get_template_directory_uri(); ?>/assets/images/img3.jpg" alt="Business team working together" class="img-fluid">
       </div>
     </div>
-  <?php endforeach; ?>
+  </div>
+  <div class="slide">
+    <div class="box">
+      <div class="slide-text">
+        <h1>Trusted Partners & Suppliers</h1>
+        <p>From sourcing to delivery, we connect you with the best in the industry.</p>
+      </div>
+      <div class="slide-image">
+        <img src="<?php get_template_directory_uri(); ?>/assets/images/img1.jpg" alt="Supplier logistics" class="img-fluid">
+      </div>
+    </div>
+  </div>
+  <div class="slide">
+    <div class="box">
+      <div class="slide-text">
+        <h1>Fast & Reliable Delivery</h1>
+        <p>Optimize your operations with seamless supply chain support.</p>
+      </div>
+      <div class="slide-image">
+        <img src="<?php get_template_directory_uri(); ?>/assets/images/img2.jpg" alt="Logistics and shipping" class="img-fluid">
+      </div>
+    </div>
+  </div>
 </div>
